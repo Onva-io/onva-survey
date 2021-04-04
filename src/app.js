5c4df745853b9e5f0d58afbc3207a1c2c409fa6f
Matt Wilson <codebrewery@gmail.com>
2021-04-04 10:50:56 +0100

    hitting home page will just redirect to onva.io

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import { Component } from 'react';
 import {
-      BrowserRouter as Router,
-      Switch,
-      Route,
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Redirect,
 } from "react-router-dom";
 import NotFound from './views/not-found';
 import Serve from './views/serve';
@@ -14,6 +15,7 @@ class App extends Component {
             <div>
                 <Router>
                     <Switch>
+                        <Route exact path="/" render={() => (window.location = process.env.REACT_APP_API_BASE.replace('api', 'www'))} />
                         <Route path="/:surveyUuid">
                             <Serve />
                         </Route>
