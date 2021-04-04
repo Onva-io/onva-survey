e5bee463df5e5d29b350342fd1845372723ea44f
Matt Wilson <codebrewery@gmail.com>
2021-04-04 10:50:37 +0100

    fallback completion text for now

--- src/components/complete.js
+++ src/components/complete.js
@@ -6,12 +6,14 @@ class SurveyComplete extends Component {
         const survey = this.props.submission.survey;
         const locale = getLocale(this.props.locale, survey.locales);
 
+        const completeText = locale.post_text || "All finished!";
+
         return (
             <div className="survey complete">
                 <h1>{locale.title}</h1>
 
                 <p>
-                    {locale.post_text}
+                    {completeText}
                 </p>
             </div>
         );
